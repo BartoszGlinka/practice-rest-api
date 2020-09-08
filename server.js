2d2c591f5c9e7eedeaa4f470f9e2c8fc88bea269
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-09-08 21:17:00 +0200

    add script build in package.json

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const concerts = require('./routes/concerts.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(cors());
+
 
 app.use('/api', testimonials); 
 app.use('/api', seats);
