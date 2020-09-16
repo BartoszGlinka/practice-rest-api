1210574dadf0b0ef250880e8abfb291a1e8c9c5e
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-09-16 21:00:07 +0200

    add function counterSeats in SeatChooser.js

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -22,7 +22,11 @@ class SeatChooser extends React.Component {
 
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
   }
-
+  counterSeats = () => {
+    const { seats, chosenDay } = this.props;
+    
+    return seats.filter(item => item.day === chosenDay).length
+  }
   prepareSeat = (seatId) => {
     const { chosenSeat, updateSeat } = this.props;
     const { isTaken } = this;
@@ -34,13 +38,13 @@ class SeatChooser extends React.Component {
 
   render() {
 
-    const { prepareSeat } = this;
-    const { requests, seats } = this.props;
+    const { prepareSeat, counterSeats } = this;
+    const { requests } = this.props;
     
     return (
       <div>
         <h3>Pick a seat</h3>
-        <p>Liczba miejsc: {seats.day}</p>
+        <p>Free seats:{ 50 - counterSeats() + '/' + 50 }  </p>
         <small id="pickHelp" className="form-text text-muted ml-2"><Button color="secondary" /> – seat is already taken</small>
         <small id="pickHelpTwo" className="form-text text-muted ml-2 mb-4"><Button outline color="primary" /> – it's empty</small>
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
